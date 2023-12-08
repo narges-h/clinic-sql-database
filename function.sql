ab802abe24c61bb876e0d4f26aa3b0cc1951d5bf
narges-h <narges.80.hoseyni@gmail.com>
2023-12-09 01:23:51 +0330

    sp-function

---
+++ function.sql
@@ -0,0 +1,22 @@
+﻿
+--فانکشنی که کد ملی بیمار رو بگیره و ساعت و روز نوبتشو بده
+
+CREATE FUNCTION visit_time (@codemeli VARCHAR(10))
+RETURNS NVARCHAR(50)
+AS
+BEGIN
+    DECLARE @result NVARCHAR(255)
+    SET @result = (
+	    -- تضمین می‌کند که فقط یک مقدار از زیرپرس و جو برگردانده می‌شود.
+        SELECT TOP 1 Time 
+        FROM visit 
+        INNER JOIN patient ON visit.idParvande = Patient.idParvande
+        WHERE codeMeli = @codemeli
+    )
+    RETURN @result
+END
+
+SELECT dbo.visit_time('0592556898');
+
+
+
---
+++ sp.sql
@@ -0,0 +1,66 @@
+﻿  --ویزیت شدند و بیمه ی آسیا دارندd1 تا d2 کدملی بیمارانی که در بازه ی 
+  CREATE PROCEDURE sp_asia
+  @date1 VARCHAR(10),
+  @date2 VARCHAR(10),
+  @result nvarchar(50) output
+AS
+BEGIN
+  set @result = ( SELECT codeMeli
+  FROM Patient 
+  INNER JOIN visit ON Patient.idParvande = visit.idParvande
+  INNER JOIN bimehShode ON visit.idParvande = bimehShode.idParvande 
+  INNER JOIN bimeh ON bimehShode.codeBimeh = bimeh.codeBimeh
+  WHERE bimeh.onvan LIKE 'asia' 
+  AND visit.Date BETWEEN @date1 AND @date2)
+END
+
+declare @res NVARCHAR(50)
+exec sp_asia '1402/05/05','1402/06/26',@res output 
+select @res
+
+
+--پروسیجری که روز هفته رو بگیره وفامیلی دکترهای اون روزو بده
+
+CREATE procedure sp_barname
+@roz int ,
+@res NVARCHAR(50) output
+AS
+BEGIN
+	    select distinct lname,Profession from Doctor inner join barnamePezashk on Doctor.idD = barnamePezashk.idD
+		where daysWeek = @roz
+END
+
+Declare @res NVarChar(50)
+Exec sp_barname 2, @res output
+Print(@res)
+
+
+--CREATE PROCEDURE sp_asia2
+--    @date1 VARCHAR(10),
+--    @date2 VARCHAR(10),
+--    @result NVARCHAR(50) OUTPUT
+--AS
+--BEGIN
+--    SELECT Patient.idParvande, fname, lname 
+--    INTO #tempTable
+--    FROM Patient 
+--    INNER JOIN visit ON Patient.idParvande = visit.idParvande
+--    INNER JOIN bimehShode ON visit.idParvande = bimehShode.idParvande 
+--    INNER JOIN bimeh ON bimehShode.codeBimeh = bimeh.codeBimeh
+--    WHERE bimeh.onvan LIKE 'asia' 
+--    AND visit.Date BETWEEN @date1 AND @date2
+
+--    SET @result = (
+--        SELECT idParvande, fname, lname 
+--        FROM #tempTable
+--    )
+
+--    DROP TABLE #tempTable
+--END
+
+--DECLARE @output1 NVARCHAR(50), @output2 INT, @output3 VARCHAR(20)
+
+--EXEC sp_asia2 '2022-01-01', '2022-12-31', @output1 OUTPUT
+
+---- Retrieve the output values
+--SELECT @output1 AS Result
