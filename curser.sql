ed8795cafa3aece95f1fc8faeb55bc5d8c16c277
narges-h <narges.80.hoseyni@gmail.com>
2023-12-22 14:08:09 +0330

    curser

--- curser.sql
+++ curser.sql
@@ -1,5 +1,5 @@
-use DB_clinic GO
-
+﻿use DB_clinic GO
+--کرسری که در آن آپدیت انجام شود
 Declare @Date varchar(10)
 Declare cr Cursor
 For(
@@ -12,26 +12,32 @@ Open cr
      while(@@FETCH_STATUS=0)
           begin
           update visit
-          Set Date='1401/06/06'
+          Set Date='1401/06/07'
           where codeBimehShode = 6 
           fetch next from cr into @Date
           end
 Close cr
 Deallocate cr
+select * from visit
 
 --------------------------------------------------------
+--نمایش ایدی پرونده با نام خانوادگی بیماران زن
 Declare @id int , @name nvarchar(15)
 Declare CrName cursor
 for
 (
-			select CategoryID , CategoryName
-			from Categories
+	select  idParvande, lname
+	from Patient where gender = 0
 )
 open CrName
-			fetch next from CrName Into @id,@name
-			Print(N'Information is: '+' '+cast(@id as nvarchar(5))+' '+@name)
+fetch next from CrName Into @id,@name
+while(@@FETCH_STATUS=0)
+    begin
+		 Print(N'Information is: '+' '+cast(@id as nvarchar(5))+' '+@name)
+		 fetch next from CrName Into @id,@name
+	end
 		
 Close CrName
 Deallocate CrName
-select * from visit
+select * from Patient
 --------------------------------------------------------------
